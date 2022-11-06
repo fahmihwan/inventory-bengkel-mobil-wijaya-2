e92c2a865d49854e711d61325d8bc87a0fdb6d9a
fahmihwan <fahmiiwan86@gmail.com>
2022-11-06 22:24:26 +0700

    revisi3

--- templastes/topbar.php
+++ templastes/topbar.php
@@ -7,9 +7,12 @@
     <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
 
     </form>
-    <?= $varSession['username'] ?>
+
     <!-- Navbar-->
-    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+    <ul class="navbar-nav ms-auto  ms-md-0 me-3 me-lg-4">
+        <div class=" d-flex justify-content-center align-items-center">
+            <?= $varSession['username'] ?>
+        </div>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
